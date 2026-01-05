0a29f59b7d90d2f4a430f9637c96e6f81862f03a
liabenyehonatan <liabenyehonatan@gmail.com>
2026-01-05 08:55:28 +0200

    HW8 Lia

--- User.java
+++ User.java
@@ -104,7 +104,7 @@ public class User {
 
     /**
      * Counts the number of users that both this user and the other user follow.
-     * /* Notice: This is the size of the intersection of the two follows lists.
+     * Notice: This is the size of the intersection of the two follows lists.
      */
     public int countMutual(User other) {
         int count = 0;
